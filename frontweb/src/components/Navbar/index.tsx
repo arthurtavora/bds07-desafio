37913f20b8fceeb32ec60c9e88b397e13abf698e
arthurtavora <arthurtavorademello@gmail.com>
2021-12-12 19:07:38 -0300

    Create Navbar hamburguer menu

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,13 +1,27 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-md navbar-light bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscars-navbar"
+          aria-controls="dscars-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscars-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
               <a href="link">
